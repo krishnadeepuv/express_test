aaa0eae3d5004857a5ad93ebe1499af89fec4df9
krishnadeepu vuppala <krishnadeepuv@krishnadeepus-MacBook-Air.local>
2022-01-23 15:03:48 -0600

    404 and route filter

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const express = require('express');
 const body_parser = require('body-parser');
 const admin_route = require('./router/admin.js');
 const shop_route = require('./router/shop.js');
+const { response } = require('express');
 
 const app = express();
 
 app.use(body_parser.urlencoded({ extended: false }));
 
-app.use(admin_route);
+app.use('/admin', admin_route);
 app.use(shop_route);
 
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>")
+})
 app.listen(3000);
